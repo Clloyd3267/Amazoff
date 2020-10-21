bd89b3f3994702c7d4d48603c20cf2067b50656d
kylebielby <kbielby09@gmail.com>
2020-10-21 13:43:00 -0400

    added new View class to load new view dynamically

---
+++ app/src/main/java/com/example/amazoff/newView.java
@@ -0,0 +1,11 @@
+package com.example.amazoff;
+
+import android.content.Context;
+import android.view.View;
+
+public class newView extends View {
+
+    public newView(Context context) {
+        super(context);
+    }
+}
