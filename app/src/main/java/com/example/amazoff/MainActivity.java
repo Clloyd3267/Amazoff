1a50ea95a155118512f0bfc8de2eb012c0723da1
kylebielby <kbielby09@gmail.com>
2020-10-27 23:25:52 -0400

    added grid view with product images

--- app/src/main/java/com/example/amazoff/MainActivity.java
+++ app/src/main/java/com/example/amazoff/MainActivity.java
@@ -10,26 +10,48 @@ import android.util.DisplayMetrics;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RelativeLayout relativeLayout;
+    private DatabaseManager dataMan;
+    private ArrayList<Product> products;
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dataMan = new DatabaseManager(this);    //create new database
 
+        //create bitmap objects for product images
+        Bitmap xbox = BitmapFactory.decodeResource(getResources(), R.drawable.xbox_one_s);
+        Bitmap playstation = BitmapFactory.decodeResource(getResources(), R.drawable.playstation_5);
+        Bitmap gamingComp = BitmapFactory.decodeResource(getResources(), R.drawable.gaming_computer);
 
-        
+        //create Products
+        products = new ArrayList<Product>();
+
+        products.add(new Product("xbox","crap",2,5.00, xbox));
+        products.add(new Product("playstation","junk",2,10.00, playstation));
+        products.add(new Product("computer","stuff",2,12.00, gamingComp));
+
+        for(Product prod : products) {
+            dataMan.storeProduct(prod);
+        }
+
+        dataMan.getProducts();
 
         ImageView img = new ImageView(this);
         img.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.xbox_one_s));
 //        productImages.addView(img);
 
         //set image
-        relativeLayout = findViewById(R.id.image_view_holder);
-        relativeLayout.addView(img);
+//        relativeLayout = findViewById(R.id.image_view_holder);
+//        relativeLayout.addView(img);
 
         updateView();
 
@@ -43,10 +65,14 @@ public class MainActivity extends AppCompatActivity {
 
     //temporary
     public void updateView() {
-        relativeLayout.removeAllViewsInLayout();    //removes previous view
+//        relativeLayout.removeAllViewsInLayout();    //removes previous view
+        RelativeLayout thisLayout = new RelativeLayout(this);
 
-        //TODO test product remove later
-//        Product newProd = new Product("xbox", "junk", 1, 5.00, stuff);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
 
         //TODO temporarly create bitmap objects
         Bitmap xbox = BitmapFactory.decodeResource(getResources(), R.drawable.xbox_one_s);
@@ -55,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
         //create grid layout for products
         GridLayout productImages = new GridLayout(this);    //store images in grid view
+        ScrollView scrollView = new ScrollView(this);
         productImages.setRowCount(2);
         productImages.setColumnCount(10);    //TODO change number of columns for number of images
 
@@ -68,9 +95,9 @@ public class MainActivity extends AppCompatActivity {
         playStationImage.setImageBitmap(playstation);
         computerImage.setImageBitmap(gamingComp);
 
-        productImages.addView(xboxImage);
-        productImages.addView(playStationImage);
-        productImages.addView(computerImage);
+        productImages.addView(xboxImage, width , height / 3);
+        productImages.addView(playStationImage, width , 2 * (height / 3));
+        productImages.addView(computerImage, width , 3 * (height / 3));
 
 //        for(Product prod : products) {
 //            ImageView img = new ImageView(this);
@@ -80,6 +107,9 @@ public class MainActivity extends AppCompatActivity {
 //            productImages.addView(img);
 //        }
 
-        relativeLayout.addView(productImages);
+        scrollView.addView(productImages);
+        thisLayout.addView(scrollView);
+        setContentView(thisLayout);
+
     }
 }
