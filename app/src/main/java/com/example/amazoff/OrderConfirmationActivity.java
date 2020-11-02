9d2655ddc316dfc135433d277ef3d7b432977126
kylebielby <kbielby09@gmail.com>
2020-11-02 10:29:31 -0500

    changed cart icon background color and changed the details view to a scroll view

--- app/src/main/java/com/example/amazoff/OrderConfirmationActivity.java
+++ app/src/main/java/com/example/amazoff/OrderConfirmationActivity.java
@@ -3,8 +3,16 @@ package com.example.amazoff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderConfirmationActivity extends AppCompatActivity {
+    private DatabaseManager dataMan;
+    private TextView streetInfo;
+    private TextView zipCodeInfo;
+    private TextView cardNumber;
+    private TextView cardName;
+    private TextView expirationDate;
+    private TextView cvcNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/amazoff/TestCartModel.java
@@ -0,0 +1,19 @@
+package com.example.amazoff;
+
+import android.content.Intent;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class TestCartModel implements Serializable {
+//    private ArrayList<Integer> selectedProducts;
+    private int[][] productSelections;
+
+    public TestCartModel(ArrayList<Integer> productIDs) {
+//        for(Integer) {
+//
+//        }
+//        this.selectedProducts = productIDs;
+    }
+
+}
